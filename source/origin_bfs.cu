eda5edb300ac3fe2fddc93503344b5a83d6ee4c1
Daniel Simon <Dan.Simon@parsons.com>
2020-08-09 15:44:25 -0700

    Integrated yubins_bfs with fixes

--- source/origin_bfs.cu
+++ source/origin_bfs.cu
@@ -8,13 +8,15 @@
 #define BLOCK_QUEUE_SIZE 16
 #define BLOCK_SIZE 8.0
 
-#define CHECK_ERROR(call) { \
-cudaError_t err = call; \
-if (err != cudaSuccess) { \
-printf("%s in %s at line %d\n", cudaGetErrorString(err), __FILE__, __LINE__); \
-exit(err); \
-} \
-}
+//#define CudaCatchError(call) { \
+//cudaError_t err = call; \
+//if (err != cudaSuccess) { \
+//printf("%s in %s at line %d\n", cudaGetErrorString(err), __FILE__, __LINE__); \
+//exit(err); \
+//} \
+//}
+
+#include "error_checker.h"
 
 __global__ void frontier_init_kernel(int* p_frontier_tail_d, int* c_frontier_tail_d, int* p_frontier_d, int* visited_d, int* label_d, int source) {
   visited_d[source] = 1;
@@ -48,7 +50,7 @@ __global__ void BFS_Bqueue_kernel(int* p_frontier, int* p_frontier_tail, int* c_
     for (int i = edges[my_vertex]; i < edges[my_vertex + 1]; ++i) {
       const int was_visited = atomicExch(&(visited[dest[i]]), 1);
       if (not was_visited) {
-        label[dest[i]] = label[my_vertex];
+        label[dest[i]] = label[my_vertex] + 1;
         const int my_tail = atomicAdd(&c_frontier_tail_s, 1);
         if (my_tail < BLOCK_QUEUE_SIZE) {
           c_frontier_s[my_tail] = dest[i];
@@ -76,35 +78,39 @@ void BFS_host(int source, int *edges, int *dest, int *label, int edges_num, int 
 
   int *edges_d, *dest_d, *label_d, *visited_d;
 
-  CHECK_ERROR(cudaMalloc((void**)&edges_d, (vertex_num + 1) * sizeof(int)));
-  CHECK_ERROR(cudaMalloc((void**)&dest_d, edges_num * sizeof(int)));
-  CHECK_ERROR(cudaMalloc((void**)&label_d, vertex_num * sizeof(int)));
-  CHECK_ERROR(cudaMalloc((void**)&visited_d, vertex_num * sizeof(int)));
+  CudaCatchError(cudaMallocManaged((void**)&edges_d, (vertex_num + 1) * sizeof(int)));
+  CudaCatchError(cudaMallocManaged((void**)&dest_d, edges_num * sizeof(int)));
+  CudaCatchError(cudaMallocManaged((void**)&label_d, vertex_num * sizeof(int)));
+  CudaCatchError(cudaMallocManaged((void**)&visited_d, vertex_num * sizeof(int)));
 
-  cudaMemcpy(edges_d, edges, (edges_num * sizeof(int)), cudaMemcpyHostToDevice);
-  cudaMemcpy(dest_d, dest, ((vertex_num + 1) * sizeof(int)), cudaMemcpyHostToDevice);
-  cudaMemcpy(label_d, label, (vertex_num * sizeof(int)), cudaMemcpyHostToDevice);
+  CudaCatchError(cudaMemcpy(edges_d, edges, (vertex_num + 1) * sizeof(int), cudaMemcpyHostToDevice));
+  CudaCatchError(cudaMemcpy(dest_d, dest, edges_num * sizeof(int), cudaMemcpyHostToDevice));
+  CudaCatchError(cudaMemcpy(label_d, label, vertex_num * sizeof(int), cudaMemcpyHostToDevice));
 
   int *frontier_d, *c_frontier_tail_d, *p_frontier_tail_d;
-  CHECK_ERROR(cudaMalloc((void**)&frontier_d, 2 * MAX_FRONTIER_SIZE * sizeof(int)));
-  int *c_frontier_d = &frontier_d[0];
-  int *p_frontier_d = &frontier_d[MAX_FRONTIER_SIZE];
-  
-  cudaMemset(visited_d, 0, vertex_num * sizeof(int));
+  CudaCatchError(cudaMallocManaged((void**)&frontier_d, 2 * MAX_FRONTIER_SIZE * sizeof(int)));
+  CudaCatchError(cudaMallocManaged((void**)&c_frontier_tail_d, sizeof(int)));
+  CudaCatchError(cudaMallocManaged((void**)&p_frontier_tail_d, sizeof(int)));
+  int *c_frontier_d = frontier_d;
+  int *p_frontier_d = frontier_d + MAX_FRONTIER_SIZE;
+
+  CudaCatchError(cudaMemset(visited_d, 0, vertex_num * sizeof(int)));
   printf("1\n");
-  frontier_init_kernel <<< 1,1 >>>(p_frontier_tail_d, c_frontier_tail_d, p_frontier_d, visited_d, label_d, source); 
+  CudaCatchError(cudaGetLastError());
+  frontier_init_kernel <<< 1,1 >>>(p_frontier_tail_d, c_frontier_tail_d, p_frontier_d, visited_d, label_d, source);
+  CudaCatchError(cudaGetLastError());
   int p_frontier_tail = 1;
   printf("2\n");
   while (p_frontier_tail > 0) {
     int num_blocks = ceil(p_frontier_tail / BLOCK_SIZE);
     BFS_Bqueue_kernel <<< num_blocks, BLOCK_SIZE >>> (p_frontier_d, p_frontier_tail_d, c_frontier_d, c_frontier_tail_d, edges_d, dest_d, label_d, visited_d);
-    cudaMemcpy(&p_frontier_tail, c_frontier_tail_d, sizeof(int), cudaMemcpyDeviceToHost); 
-	int *temp = c_frontier_d; c_frontier_d = p_frontier_d; p_frontier_d = temp;
-	printf("3\n");
-	frontier_tail_swap_kernel <<< 1,1 >>>(p_frontier_tail_d, c_frontier_tail_d); 
-	printf("4\n");
-
-
+    CudaCatchError(cudaGetLastError());
+    CudaCatchError(cudaMemcpy(&p_frontier_tail, c_frontier_tail_d, sizeof(int), cudaMemcpyDeviceToHost));
+    int *temp = c_frontier_d; c_frontier_d = p_frontier_d; p_frontier_d = temp;
+    printf("3\n");
+    frontier_tail_swap_kernel <<< 1,1 >>>(p_frontier_tail_d, c_frontier_tail_d);
+    CudaCatchError(cudaGetLastError());
+    printf("4\n");
   }
   cudaMemcpy(label, label_d, vertex_num * sizeof(int), cudaMemcpyDeviceToHost);
 
@@ -120,7 +126,7 @@ int main(int argc, char* argv[]) {
 
   int _edges[] = {0, 2, 4, 7, 9, 11, 12, 13, 15, 15};
   int _dest[] = {1, 2, 3, 4, 5, 6, 7, 4, 8, 5, 8, 6, 8, 0, 6};
-  int _label[] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+  int _label[] = {-1, -1, -1, -1, -1, -1, -1, -1, -1};
   
   int* edges  = _edges;
   int* dest  = _dest;
